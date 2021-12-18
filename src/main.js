97d1626f94ff63431e2f96264e6c71c643f90de6
SoniRat <Soniesen.Ratnasingam@htwg-konstanz.de>
2021-12-18 14:49:29 +0100

    pwa running via live server

--- src/main.js
+++ src/main.js
@@ -5,6 +5,22 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import 'bootstrap/dist/css/bootstrap.css'
+import $ from 'jquery'
+import 'jquery/src/jquery.js'
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(async function() {
+    try{
+      var res = await fetch(event.request);
+      var cache = await caches.open('cache');
+      cache.put(event.request.url, res.clone());
+      return res;
+    }
+    catch(error){
+      return caches.match(event.request);
+    }
+  }());
+});
 
 Vue.config.productionTip = false
 
