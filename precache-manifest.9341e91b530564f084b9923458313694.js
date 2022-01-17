2e8deb76de890122ca0dcb7ba9c82085e31a83fd
BenjaminMannsdoerfer <b.mannsdoerfer@gmx.de>
2022-01-17 14:47:51 +0100

    deploy

---
+++ precache-manifest.9341e91b530564f084b9923458313694.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30e7035bfeb306d8fc35",
+    "url": "/stratego-vue-app/css/about.51986561.css"
+  },
+  {
+    "revision": "8e12d707288affb4c771",
+    "url": "/stratego-vue-app/css/app.08007df0.css"
+  },
+  {
+    "revision": "4b805cd9635f322d2010",
+    "url": "/stratego-vue-app/css/chunk-vendors.bc86c6ac.css"
+  },
+  {
+    "revision": "0b6f0d0f72ed106ab5ba319ac1056e6c",
+    "url": "/stratego-vue-app/img/Stratego_plateau.0b6f0d0f.jpg"
+  },
+  {
+    "revision": "1d59feabb6e733ce74c45497361f651d",
+    "url": "/stratego-vue-app/img/game_screen.1d59feab.png"
+  },
+  {
+    "revision": "3c23f393b0908b0c3b73e433e4d9a60f",
+    "url": "/stratego-vue-app/img/matchfield.3c23f393.png"
+  },
+  {
+    "revision": "0bbdbb85397a74929a462ef57608c5f1",
+    "url": "/stratego-vue-app/img/matchfield_400x400.0bbdbb85.png"
+  },
+  {
+    "revision": "7dd1f32c2596fcde9f2b2be98aed5b77",
+    "url": "/stratego-vue-app/img/move_screen.7dd1f32c.png"
+  },
+  {
+    "revision": "906370f6f181169c08b6425f1b2f9387",
+    "url": "/stratego-vue-app/img/start_screen.906370f6.png"
+  },
+  {
+    "revision": "0be2df471c19f72b7ddab7a7032ad665",
+    "url": "/stratego-vue-app/img/stratego-bomb.0be2df47.svg"
+  },
+  {
+    "revision": "c32217260978f0548353daa932148945",
+    "url": "/stratego-vue-app/img/stratego-captain.c3221726.svg"
+  },
+  {
+    "revision": "8b866d5774f4d0f53b4cd0bb666bd9ef",
+    "url": "/stratego-vue-app/img/stratego-colonel.8b866d57.svg"
+  },
+  {
+    "revision": "1bac504bbeaefd4e0e300184f1671c04",
+    "url": "/stratego-vue-app/img/stratego-flag.1bac504b.svg"
+  },
+  {
+    "revision": "376826a9784209d53d7412c11bb806a9",
+    "url": "/stratego-vue-app/img/stratego-general.376826a9.svg"
+  },
+  {
+    "revision": "811a3054a97f58e13ab392d464d5b471",
+    "url": "/stratego-vue-app/img/stratego-lieutenant.811a3054.svg"
+  },
+  {
+    "revision": "3eae07f4e57323d0b9de2d45d7740a1b",
+    "url": "/stratego-vue-app/img/stratego-major.3eae07f4.svg"
+  },
+  {
+    "revision": "6b499ee749f8170ea49cea76e79a61c3",
+    "url": "/stratego-vue-app/img/stratego-marshal.6b499ee7.svg"
+  },
+  {
+    "revision": "d0b3d1a45e78c0356feb63f312a47a7d",
+    "url": "/stratego-vue-app/img/stratego-miner.d0b3d1a4.svg"
+  },
+  {
+    "revision": "b88df5170a182d1ae2477f440664c097",
+    "url": "/stratego-vue-app/img/stratego-scout.b88df517.svg"
+  },
+  {
+    "revision": "1ed3e5ead72a4c912a42a9bca3b7350a",
+    "url": "/stratego-vue-app/img/stratego-sergeant.1ed3e5ea.svg"
+  },
+  {
+    "revision": "9658c5f20f672aceb12ef9a3473801d8",
+    "url": "/stratego-vue-app/img/stratego-spy.9658c5f2.svg"
+  },
+  {
+    "revision": "1c0ced3d805f535148364138d793af52",
+    "url": "/stratego-vue-app/img/stratego_icons/android-144x144.png"
+  },
+  {
+    "revision": "40f1ff9bf9d066da618e0f21a3a52b6c",
+    "url": "/stratego-vue-app/img/stratego_icons/android-192x192.png"
+  },
+  {
+    "revision": "83819d5285925b5b14bfdaea3ecda6e4",
+    "url": "/stratego-vue-app/img/stratego_icons/android-36x36.png"
+  },
+  {
+    "revision": "c3d5a1a903bb98236709a672b3d91729",
+    "url": "/stratego-vue-app/img/stratego_icons/android-48x48.png"
+  },
+  {
+    "revision": "b35e79c7521744941eff8962c7e1a481",
+    "url": "/stratego-vue-app/img/stratego_icons/android-72x72.png"
+  },
+  {
+    "revision": "37bc5043fafeb9f9549faaf74bbbdf08",
+    "url": "/stratego-vue-app/img/stratego_icons/android-96x96.png"
+  },
+  {
+    "revision": "40f1ff9bf9d066da618e0f21a3a52b6c",
+    "url": "/stratego-vue-app/img/stratego_icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "7054da62194681cdc0bcf55601a23fa2",
+    "url": "/stratego-vue-app/img/stratego_icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "40f1ff9bf9d066da618e0f21a3a52b6c",
+    "url": "/stratego-vue-app/img/stratego_icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "7054da62194681cdc0bcf55601a23fa2",
+    "url": "/stratego-vue-app/img/stratego_icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "b31034dcb2c62eaeda7114fa5087efce",
+    "url": "/stratego-vue-app/img/stratego_icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "e0ece25216c2269df474cda84eca5d24",
+    "url": "/stratego-vue-app/img/stratego_icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "b00ef878334c183bbe2df3d46698b679",
+    "url": "/stratego-vue-app/img/stratego_icons/apple-touch-icon-167x167.png"
+  },
+  {
+    "revision": "99efe48a3c621b7571bbb3145f54baf9",
+    "url": "/stratego-vue-app/img/stratego_icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "2e68ca0f8491e7b3b464cf6a03404cb4",
+    "url": "/stratego-vue-app/img/stratego_icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "dca148581a40278172d5ff28da453322",
+    "url": "/stratego-vue-app/img/stratego_icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "a77f7841e5c4936a52d28da567267492",
+    "url": "/stratego-vue-app/img/stratego_icons/favicon-128x128.png"
+  },
+  {
+    "revision": "427c69ba86f202c35fb033a44bb75d6b",
+    "url": "/stratego-vue-app/img/stratego_icons/favicon-16x16.png"
+  },
+  {
+    "revision": "65795a9c98b91d1b6c28c7443209effa",
+    "url": "/stratego-vue-app/img/stratego_icons/favicon-256x256.png"
+  },
+  {
+    "revision": "11ea855bd907a9d81aa5da7f1c254b39",
+    "url": "/stratego-vue-app/img/stratego_icons/favicon-32x32.png"
+  },
+  {
+    "revision": "c3d5a1a903bb98236709a672b3d91729",
+    "url": "/stratego-vue-app/img/stratego_icons/favicon-48x48.png"
+  },
+  {
+    "revision": "5a2b853c79ce49ac38621b4d5f27f361",
+    "url": "/stratego-vue-app/img/stratego_icons/favicon-64x64.png"
+  },
+  {
+    "revision": "37bc5043fafeb9f9549faaf74bbbdf08",
+    "url": "/stratego-vue-app/img/stratego_icons/favicon-96x96.png"
+  },
+  {
+    "revision": "3ae125e1326bc575e2e43d363b01bebf",
+    "url": "/stratego-vue-app/img/stratego_icons/icon-background.png"
+  },
+  {
+    "revision": "e65fc023656d87dea1d12ebcb12973e2",
+    "url": "/stratego-vue-app/img/stratego_icons/icon-foreground.png"
+  },
+  {
+    "revision": "e532f0f150fcbaaa05ccc0968f9b115c",
+    "url": "/stratego-vue-app/img/stratego_icons/icon.png"
+  },
+  {
+    "revision": "1c0ced3d805f535148364138d793af52",
+    "url": "/stratego-vue-app/img/stratego_icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "1c0ced3d805f535148364138d793af52",
+    "url": "/stratego-vue-app/img/stratego_icons/mstile-144x144.png"
+  },
+  {
+    "revision": "f155e9a2b2cfcd5b8ca065c5a8b006dd",
+    "url": "/stratego-vue-app/img/stratego_icons/mstile-150x150.png"
+  },
+  {
+    "revision": "3b98b50fe5f90f3139c5cc9d0b0d7868",
+    "url": "/stratego-vue-app/img/stratego_icons/splash.png"
+  },
+  {
+    "revision": "003f1224cb47dddac88957138ea6cf8b",
+    "url": "/stratego-vue-app/img/stratego_icons/tile150x150.png"
+  },
+  {
+    "revision": "21062fa03b4ecdb149042621ab4ad6bb",
+    "url": "/stratego-vue-app/img/stratego_icons/tile310x150.png"
+  },
+  {
+    "revision": "2ffeccdfe78557ff9a46af3ac5d38b46",
+    "url": "/stratego-vue-app/img/stratego_icons/tile310x310.png"
+  },
+  {
+    "revision": "a77f7841e5c4936a52d28da567267492",
+    "url": "/stratego-vue-app/img/stratego_icons/tile70x70.png"
+  },
+  {
+    "revision": "a60c539bbf022995b260ca6f445d1683",
+    "url": "/stratego-vue-app/img/stratego_start.a60c539b.png"
+  },
+  {
+    "revision": "4e7e4670503c0782bedfa9d8b58a7ae0",
+    "url": "/stratego-vue-app/img/stratego_title.4e7e4670.png"
+  },
+  {
+    "revision": "0878e4ba1a84ee09fa3b2d60a8b32763",
+    "url": "/stratego-vue-app/index.html"
+  },
+  {
+    "revision": "30e7035bfeb306d8fc35",
+    "url": "/stratego-vue-app/js/about.8d566257.js"
+  },
+  {
+    "revision": "8e12d707288affb4c771",
+    "url": "/stratego-vue-app/js/app.04c08008.js"
+  },
+  {
+    "revision": "4b805cd9635f322d2010",
+    "url": "/stratego-vue-app/js/chunk-vendors.8bd7c586.js"
+  },
+  {
+    "revision": "61df1317c5443c61d0622bc1db69207e",
+    "url": "/stratego-vue-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/stratego-vue-app/robots.txt"
+  }
+]);
